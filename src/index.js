a59bdb9f344a2782990ecede2778f4028b3f914d
Le Minh Quang <quanglm1998@gmail.com>
2021-01-06 13:22:20 +0700

    update algo

--- src/index.js
+++ src/index.js
@@ -182,3 +182,52 @@ function drawMarker(lat, lon) {
   marker = L.marker([lat, lon]).addTo(map);
 }
 
+async function solve() {
+  let foo = await httpGet("/routes/route_99.json");
+  let now = JSON.parse(foo);
+
+  let bar = now['forward'][0]['coordRoute'];
+  // console.log(bar[Object.keys(bar)[0]]);
+  // var firstKey = Object.keys(myObject)[0];
+
+  var latlngs = []
+
+  bar[Object.keys(bar)[0]].forEach(element => {
+    latlngs.push([element.Latitude, element.Longitude]);
+    // drawMarker(element.Lat, element.Lng);
+    // L.circle([element.Latitude, element.Longitude], {
+    //       color: 'red',
+    //       fillColor: '#f03',
+    //       fillOpacity: 0.5,
+    //       radius: 20
+    //   }).addTo(map);
+  });
+
+  var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+
+
+  // now['forward'][1]['stops'].forEach(element => {
+  //   // drawMarker(element.Lat, element.Lng);
+  //   L.circle([element.Lat, element.Lng], {
+  //         color: 'blue',
+  //         fillColor: '#f03',
+  //         fillOpacity: 0.5,
+  //         radius: 20
+  //     }).addTo(map);
+  // });
+  // console.log(now);
+
+}
+
+// solve();
+
+// async function main() {
+//   await init();
+//   startingPoint = L.latLng(10.770822, 106.700233);
+//   destination = L.latLng(10.785822, 106.700233);
+//   getOptimalRoutes(startingPoint, destination, (u, v) => {
+//     return u.dist < v.dist;
+//   });
+// }
+
+// main();
